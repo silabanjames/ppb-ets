93ab6ec5b6c5e37ed3322ebc4ed1222fc9314482
silabanjames <silaban.james13@gmail.com>
2024-05-16 16:49:00 +0700

    Add Card Subject's Icons and Text

--- app/src/main/java/com/example/ppbets/ui/components/dashboard/SubjectCard.kt
+++ app/src/main/java/com/example/ppbets/ui/components/dashboard/SubjectCard.kt
@@ -1,19 +1,25 @@
 package com.example.ppbets.ui.components.dashboard
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarToday
+import androidx.compose.material.icons.filled.Domain
+import androidx.compose.material.icons.filled.Schedule
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,11 +55,12 @@ fun SubjectCard() {
                 modifier = Modifier,
             )
             Row {
-                Text(text = "Rabu")
+//                Text(text = "Rabu")
+                InformationClass(icon = Icons.Default.CalendarToday, description = "Rabu")
                 Spacer(modifier = Modifier.weight(1f))
-                Text(text = "IF-305")
+                InformationClass(icon = Icons.Default.Domain, description = "IF-305")
                 Spacer(modifier = Modifier.weight(1f))
-                Text(text = "13:30 - 15:20")
+                InformationClass(icon = Icons.Default.Schedule, description = "13:30 - 15:20")
             }
         }
     }
@@ -61,9 +68,22 @@ fun SubjectCard() {
 
 @Composable
 fun InformationClass(
-    @Draw
+    icon: ImageVector,
+    description: String
 ) {
-
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            modifier = Modifier.size(18.dp)
+        )
+        Text(
+            text = description,
+            modifier = Modifier.padding(start=5.dp)
+        )
+    }
 }
 
 @Preview(
